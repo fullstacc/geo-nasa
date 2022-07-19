84f8b819523453ed41efbf8048cd99db7ac3eb5b
sulfuroussquares <bbetancova@gmail.com>
2022-07-19 14:39:07 -0600

    added tle translation service and dependencies

---
+++ src/utils/translationService.js
@@ -0,0 +1,11 @@
+import { propagate, twoline2satrec, gstime, eciToGeodetic } from 'satellite.js';
+import {useGeolocated} from "react-geolocated"
+
+
+
+// fetcher
+const tleTranslate = () => {
+   
+}
+
+export {tleTranslate}
