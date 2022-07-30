1859ee530c2ea0937edc93c82cbe64836213d3a9
sulfuroussquares <bbetancova@gmail.com>
2022-07-30 15:08:39 -0600

    refactor to meet airbnb style

--- src/components/Apod.jsx
+++ src/components/Apod.jsx
@@ -1,52 +1,39 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/function-component-definition */
+import { useEffect, useState } from 'react';
 import dataFetcher from '../utils/dataService';
-import {useEffect, useState} from 'react'
 
 const Apod = () => {
+  // APOD target
+  const apodUrl = 'https://api.nasa.gov/planetary/apod?';
+  const [apod, setApod] = useState(null);
+  const [dataLoaded, setDataLoaded] = useState(false);
 
-    // APOD target
-    const apod_url = `https://api.nasa.gov/planetary/apod?`
-    const [apod, setApod] = useState(null)
-    const [dataLoaded, setDataLoaded] = useState(false)
+  const handleApod = (apodObject) => {
+    setApod(apodObject);
+    setDataLoaded(true);
+  };
 
+  useEffect(() => {
+    dataFetcher('apod', apodUrl, handleApod, apod);
+  }, []);
 
-    const handleApod = (apodObject) => {
-        setApod(apodObject)
-        setDataLoaded(true)
-    }
+  return (
+    <div id="apod">
+      <h1>NASA Astronomy Photo of the Day </h1>
+      <h2>{dataLoaded ? (apod.date) : null} : {dataLoaded ? (apod.title) : null} </h2>
+      <b> Credit : {dataLoaded ? (apod.copyright) : null}</b>
+      <img id="apod-img" src={dataLoaded ? (apod.hdurl) : null} />
+      <p>
+        <i>{dataLoaded ? (apod.explanation) : null}</i>
+      </p>
+    </div>
+  );
+};
 
-    useEffect(() => {
-        dataFetcher('apod', apod_url, handleApod, apod);
-        // setApod({
-        //     date: responseObject.date,
-        //     explanation: responseObject.explanation,
-        //     hdurl: hdurl,
-        // })
-      }, []);
-
-      //  {Object.entries(apod).map(([key, value]) => `key ${key} value ${value}`)}
-
-    return (
-        <div id="apod">
-            <h1>NASA Astronomy Photo of the Day </h1>
-            <h2>{dataLoaded ? (apod.date) : null
-            } : {dataLoaded ? (apod.title) : null
-            } </h2>
-            
-            <b> Credit : {dataLoaded ? (apod.copyright) : null}</b>
-            <img id="apod-img" src={dataLoaded ? (apod.hdurl) : null}></img>
-            <p><i>{dataLoaded ? (apod.explanation) : null}</i>
-            
-            </p>
-            
-            
-            
-           
-        
-        </div>
-    )
-}
-
-export {Apod}
+export default Apod;
 
 // TODO: fix the async nature of getting the apod and storing it in an object
 // see: country database
