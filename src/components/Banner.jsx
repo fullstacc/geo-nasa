875efa6b25a92d2d75908ef07b28df972915a8b3
sulfuroussquares <bbetancova@gmail.com>
2022-07-31 20:20:40 -0600

    refactor to meet airbnb style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,22 +1,22 @@
-/* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable react/function-component-definition */
-/* eslint-disable react/jsx-wrap-multilines */
 import './Banner.css';
 import Apod from './Apod';
 
-const Banner = ({ bannerVisible, closeBanner }) => {
+function Banner({ bannerVisible, closeBanner }) {
   if (bannerVisible) {
     return (
       <div className="controlPanel" id="banner">
         <h1> Welcome to GEO. </h1>
-        <p>From this app you'll be able to visualize various data feeds.</p>
+        <p> From this app you&apos;ll be able to visualize various data feeds. </p>
         <p>
-          <i> NOTE: Please wait a few moments before attempting to turn on "My Location" </i>
+          <i>
+            NOTE: Please wait a few moments before attempting to turn on &quot;My Location&quot;
+          </i>
         </p>
         <Apod />
         <form onSubmit={closeBanner}><button className="banner-button" type="submit">OK</button></form>
-      </div>);
+      </div>
+    );
   }
-};
+}
 
 export default Banner;
